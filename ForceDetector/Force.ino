4738e49f3ca8b944630969865fcde0b86f414a60
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-05-06 14:41:04 +0200

    added force detector

---
+++ ForceDetector/Force.ino
@@ -0,0 +1,30 @@
+/**
+ * Force Sensor
+ *
+ * Callbacks:
+ * - force_change(float percent)
+ */
+
+#define FORCE_PIN 0
+#define FORCE_INTERVAL 100
+
+/* --------------------------------------------------- */
+
+int force_history = 0;
+long long force_last_read = 0;
+
+void force_loop() {
+  if(millis() - FORCE_INTERVAL > force_last_read) {
+    force_read();
+    force_last_read = millis();
+  }
+}
+
+void force_read() {
+  int v = analogRead(FORCE_PIN);
+  
+  if(v != force_history) {
+    force_change(constrain(map(v, 550, 1023.0, 0, 100.0), 0, 100));
+    force_history = v;
+  }
+}
