20bc097e4b45168be1d321dc09a19e420028f3f9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-05-07 19:29:07 +0200

    update

--- MotionDetector/Motion.ino
+++ MotionDetector/Motion.ino
@@ -5,12 +5,17 @@
  * - motion_change(float percent)
  */
 
-#define MOTION_PIN 0
+#define MOTION_X 0
+#define MOTION_Y 1
+#define MOTION_Z 2
 #define MOTION_INTERVAL 100
 
 /* --------------------------------------------------- */
 
-int motion_history = 0;
+int motion_x_history = 0;
+int motion_y_history = 0;
+int motion_z_history = 0;
+int motion_d_history = 0;
 long long motion_last_read = 0;
 
 void motion_loop() {
@@ -21,10 +26,25 @@ void motion_loop() {
 }
 
 void motion_read() {
-  int v = analogRead(MOTION_PIN);
+  int x = analogRead(MOTION_X);
+  int y = analogRead(MOTION_Y);
+  int z = analogRead(MOTION_Z);
   
-  if(v != motion_history) {
-    motion_change(v / 1023.0 * 100.0);
-    motion_history = v;
+  if(motion_last_read > 0) {
+    int dx = abs(motion_x_history - x);
+    int dy = abs(motion_y_history - y);
+    int dz = abs(motion_z_history - z);
+    int d = dx + dy + dz;
+    
+    if(motion_d_history != d) {
+      motion_change(constrain(map(d, 0, 500, 0, 100), 0, 100));
+    }
+    
+    motion_d_history = d;
   }
+  
+  motion_x_history = x;
+  motion_y_history = y;
+  motion_z_history = z;
+  motion_last_read = millis();
 }
